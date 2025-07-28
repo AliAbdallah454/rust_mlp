2eed29d058416c352a852a08d265e508f094133c
AliAbdallah454 <alikhaledabdallah454@gmail.com>
2025-07-28 09:56:53 +0300

    fix: fixed a bug in backprop (related to softmax and cce)

--- src/layer.rs
+++ src/layer.rs
@@ -72,7 +72,9 @@ impl Layer {
             ActivationType::Sigmoid => pre_activation.sigmoid_derivative(),
             ActivationType::Linear => Tensor::ones(pre_activation.shape.clone()),
             ActivationType::Tanh => pre_activation.tanh_derivative(),
-            ActivationType::Softmax => pre_activation.softmax_derivative()
+            // For softmax activation function, this is useless since the drivative of the cross entropy takes into considiration the 
+            // derivative of the softmax.
+            ActivationType::Softmax => Tensor::zeros_2d(pre_activation.rows(), pre_activation.cols())
         };
         
 
@@ -80,21 +82,21 @@ impl Layer {
         // dL/dz = dL/da * da/dz (we have dL/da and da/dz)
         let dz = match self.activation {
             ActivationType::Softmax => {
-                activation_derivative.mul(gradient, execution_mode)
+                gradient.clone()
             },
             _ => {
                 gradient.hadamard(&activation_derivative)
             }
         }.clone();
 
-        // Gradient w.r.t. weights: dL/dW = dL/dz * x^T
+        // Gradient w.r.t weights: dL/dW = dL/dz * x^T
         let input_t = input.transpose();
         let dw = dz.mul(&input_t, execution_mode);
         
-        // Gradient w.r.t. biases: dL/db = dL/dz
+        // Gradient w.r.t biases: dL/db = dL/dz
         let db = dz.clone();
         
-        // Gradient w.r.t. input: dL/dx = W^T * dL/dz
+        // Gradient w.r.t input: dL/dx = W^T * dL/dz
         let weights_t = self.weights.transpose();
         let dx = weights_t.mul(&dz, execution_mode);
         
